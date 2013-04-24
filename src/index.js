a4d4f1aa7e82b23230b255b949af3e81e02585a4
seon <seon.piao@gmail.com>
2013-04-24 21:14:44 +0800

    exports Flowjs as global var

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 define(function(require,exports,module){
-    module.exports = {
+    window.Flowjs = {
         Class:require('./util/class'),
         Flow:require('./flow'),
         Step:require('./step'),
